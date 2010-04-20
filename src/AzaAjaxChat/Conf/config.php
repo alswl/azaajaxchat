c18cf5ddaa5a804427a738449a3db68c8bb15fe2
alswlx <alswlx@c6b6fbfe-fabb-11de-8c8a-61c5e9addfc9>
2010-04-20 01:46:40 +0000

    初始化代码

---
+++ src/AzaAjaxChat/Conf/config.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+	'APP_DEBUG' => true,
+	'DB_TYPE' => 'mysql',
+	'DB_HOST' => 'localhost',
+	'DB_NAME' => 'aac_db',
+	'DB_USER' => 'root',
+	'DB_PWD' => 'admin',
+	'DB_PORT' => '3306',
+	'DB_PREFIX' => ''
+);
+?>
---
+++ src/AzaAjaxChat/index.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Created on 2010-04-19 20:04
+ * Author: alswl
+ */
+define("THINK_PATH", "../ThinkPHP/");
+define("APP_NAME", "AzaAjaxChat");
+define("APP_PATH", ".");
+require(THINK_PATH . "/ThinkPHP.php");
+APP::run();
+?>
