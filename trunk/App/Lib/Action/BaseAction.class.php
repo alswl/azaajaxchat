4fc0590da56d9c9e02859f21cbb528fb40424d24
alswlx <alswlx@c6b6fbfe-fabb-11de-8c8a-61c5e9addfc9>
2010-04-24 04:33:53 +0000

    登录登出跳转框架完成，登录使用了M()查询数据库

--- trunk/App/Lib/Action/BaseAction.class.php
+++ trunk/App/Lib/Action/BaseAction.class.php
@@ -16,9 +16,7 @@ class BaseAction extends Action {
 		if (!Session :: is_set(C('AAC_USER_AUTH_KEY'))) {
 
 			$goto = base64_encode($_SERVER['REQUEST_URI']);
-			Session :: set('loginMessage', '当前用户未登录，请登录后继续操作');
-
-			$this->redirect("Member/index", array ('goto' => $goto, 'hasMessage' => '1'));
+			$this->redirect("Member/index", array ('type' => 1));
 		}
 	}
 }
--- trunk/App/Lib/Action/MemberAction.class.php
+++ trunk/App/Lib/Action/MemberAction.class.php
@@ -1,37 +1,85 @@
 <?php
 class MemberAction extends Action {
-	public function index() {
 	
-		$hasMessage = $_GET['hasMessage'];
-		$message = SESSION::get('loginMessage');
+	
+
+	public function index() {
+
+		$this->testIsLogin();
+
+		$type = $_GET['type'];
 		$goto = $_GET['goto'];
-		
-//		dump($hasMessage);
-//		dump($message);
-//		dump($goto);
-		
-		$tplData = array('hasMessage' => $hasMessage,
+
+		//事先定义好的message类型
+		$message = '';
+		switch ($type) {
+			case "1" :
+				$message = "当前用户未登录，请登录后继续操作";
+		}
+
+		dump($message);
+		$tplData = array (
 			'message' => $message,
-			'goto' => $goto);
+			'goto' => $goto
+		);
 		$this->assign($tplData);
-		
-		
 		$this->display();
 	}
-	
+
 	public function login() {
 		
+		$this->testIsLogin();
+
 		$userName = $_POST['name'];
 		$userPassword = $_POST['password'];
 		$rememberMe = $_POST['rememberme'];
 		$goto = $_POST['goto'];
-		
-		dump($userName);
-		dump($userPassword);
+
 		dump($rememberMe);
 		dump($goto);
-		
-		$this->success('登录成功');
+
+		$map['login_name'] = $userName;
+		$map['user_password'] = $userPassword;
+
+		$user = M("User")->where($map)->select();
+		dump($user);
+		dump($user[0]['status']);
+		dump(base64_decode($goto));
+
+		if (isset ($user)) {
+			//用户状态为启用
+			if ('1' === $user[0]['status']) {
+				Session :: set(C('AAC_USER_AUTH_KEY'), $user[0]['id']);
+								$this->redirect(base64_decode($goto));
+			} else {
+				$tplData = array (
+					'hasMessage' => 1,
+					'message' => '登录失败, 用户帐号已经停用',
+					'goto' => $goto
+				);
+			}
+		} else {
+			$tplData = array (
+				'hasMessage' => 1,
+				'message' => '登录失败, 用户不存在或者密码错误',
+				'goto' => $goto
+			);
+		}
+		$this->assign($tplData);
+		$this->display('index');
+
+	}
+	
+	public function logout() {
+		Session :: clear();
+		$this->redirect('Index/index');
+	}
+	
+	private function testIsLogin() {
+		if (Session :: is_set(C('AAC_USER_AUTH_KEY'))) {
+
+			$this->redirect("Index/index", null, 3, '当前用户已登录，正在跳转至首页');
+		}
 	}
 
 	public function x() {
